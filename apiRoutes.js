a385a53040160196193ec56d32f7827cc318b8b7
Serhii <ser4eg.dev@gmail.com>
2025-07-29 12:15:13 +0300

    Update apiRoutes.js

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,6 @@
 // routes.js
-const API_BASE_URL = 'http://localhost:5000/api'
+// const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = 'https://teachers-backend-5szu.onrender.com'
 
 export default {
   // GET: Отримати всіх вчителів
